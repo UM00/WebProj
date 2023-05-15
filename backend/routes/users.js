f4cb696a65d651974b38fb4028514aa52336a04f
UMOO <umer.idrees.1906@gmail.com>
2023-05-15 20:41:25 +0500

    2nd Commit-Authentication Added

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const AdminUserController = require('../Controller/AdminUserController')
 const Router = express.Router();
-
-
+const { userdecode, checkAdmin } = require('../Tokens/tokens');
+Router.get("/",userdecode,checkAdmin,(req,res)=>{
+    res.status(200).json({user:user})
+})
 Router.get('/getUser',AdminUserController.getUser)
 Router.post('/createUser',AdminUserController.createUser)
 Router.delete('/:id',AdminUserController.deleteUserDetail)
-
-
 module.exports =Router;
