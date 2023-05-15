f6558631f1273c469e826c5bca37f487781554d0
UMOO <umer.idrees.1906@gmail.com>
2023-05-15 21:18:43 +0500

    Routes updation with Authentication

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,10 +2,10 @@ const express = require('express')
 const AdminUserController = require('../Controller/AdminUserController')
 const Router = express.Router();
 const { userdecode, checkAdmin } = require('../Tokens/tokens');
-Router.get("/",userdecode,checkAdmin,(req,res)=>{
-    res.status(200).json({user:user})
-})
-Router.get('/getUser',checkAdmin,AdminUserController.getUser)
+// Router.get("/",userdecode,checkAdmin,(req,res)=>{
+//     res.status(200).json({user:user})
+// })
+Router.get('/getUser',userdecode,checkAdmin,AdminUserController.getUser)
 Router.post('/createUser',AdminUserController.createUser)
-Router.delete('/:id',AdminUserController.deleteUserDetail)
+Router.delete('/delete/:id',AdminUserController.deleteUserDetail)
 module.exports =Router;
